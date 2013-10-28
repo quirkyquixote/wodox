91d7fc9c3f5644122629c6c08cc0629a65a95f85
Luis Sanz <luis.sanz@gmail.com>
2013-10-28 19:00:52 +0100

    Main: changed command line behaviour

--- src/wodox/main.c
+++ src/wodox/main.c
@@ -1,6 +1,6 @@
 /*
  * Wodox reimplemented again.
- * This code copyright (c) Luis Javier Sanz 2009 
+ * This code copyright (c) Luis Javier Sanz 2009-2013 
  */
 
 #include "../menu/menu.h"		// Menus.
@@ -18,25 +18,24 @@ static const char *str_version =
 
 static const char *str_usage = 
     "Usage: wodox --help\n"
-    "Usage: wodox --version\n"
-    "Usage: wodox --edit <file>\n"
-    "Usage: wodox [<options>] [<file>...]\n";
+    "       wodox --version\n"
+    "       wodox [<options>] --edit <file>\n"
+    "       wodox [<options>] --play <file>\n"
+    "       wodox [<options>]\n";
 
-static const char *str_help = 
-    "\n"
-    "  -h, --help		Display this information and exit\n"
-    "  -v, --version		Display version info and exit\n"
-    "  -e, --edit <file>	Edit the specified level\n"
-    "  -l, --lang <lang>	Specify language\n"
-    "      --no-audio		Disable all audio\n"
-    "      --no-music		Disable all music\n"
+static const char *str_options = 
+    "Options:\n"
+    "  -l, --lang <lang>        Specify language\n"
+    "      --no-audio           Disable all audio\n"
+    "      --no-music           Disable all music\n"
     "\n";
 
 static struct option long_options[] = {
     { "no-audio", no_argument, &media.enable_audio, 0 },
     { "no-music", no_argument, &media.enable_music, 0 },
     { "help", no_argument, 0, 'h' },
-    { "edit", no_argument, 0, 'e' },
+    { "edit", required_argument, 0, 'e' },
+    { "play", required_argument, 0, 'p' },
     { "lang", required_argument, 0, 'l' },
     { "version", no_argument, 0, 'v' },
     { 0, 0, 0, 0 }
@@ -53,19 +52,29 @@ main (int argc, char * argv[])
     int option_index = 0;
     char * lang = "en";
 
-    while ((c = getopt_long (argc, argv, "hvel:", long_options, &option_index)) != -1) {
+    init_lang (lang);
+
+    while ((c = getopt_long (argc, argv, "hve:p:l:", long_options, &option_index)) != -1) {
 	switch (c) {
 	case 0: // If this option set a flag do nothing.
             break;
 
         case 'h':
 	    fputs(str_usage, stdout);
-	    fputs(str_help, stdout);
+	    fputs(str_options, stdout);
 	    exit(EXIT_SUCCESS);
     
         case 'e':
-            mode = 1;
-	    break;
+	    media_init();
+	    edit(optarg);
+	    media_end();
+	    exit(EXIT_SUCCESS);
+    
+        case 'p':
+	    media_init();
+	    play(optarg, optarg);
+	    media_end();
+	    exit(EXIT_SUCCESS);
     
         case 'l':
             lang = optarg;
@@ -81,25 +90,17 @@ main (int argc, char * argv[])
         }
     }
 
-    init_lang (lang);
-    media_init ();
-
-    if (optind < argc) {
-	for (c = optind; c < argc; ++c) {
-	    if (mode == 0){
-              if (play (argv[c], argv[c]) == 0)
-		  break; 
-   	    } else {
-	      edit (argv[c]);
-            }
-        }
-    } else {
-	menu ();
+    if (optind != argc) {
+	fputs (str_usage, stdout);
+	exit(EXIT_FAILURE);
     }
 
-    media_end ();
+    media_init();
+    menu();
+    media_end();
+
     end_lang ();
-    return 0;
+    exit(EXIT_SUCCESS);
 }
 
 
