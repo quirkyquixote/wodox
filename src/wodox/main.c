3cf72f49c1ae55eaaf2a1c471a08c393a46d9325
Luis Sanz <luis.sanz@gmail.com>
2013-10-28 11:53:32 +0100

    Main: refactored

--- src/wodox/main.c
+++ src/wodox/main.c
@@ -10,18 +10,28 @@
 #include <getopt.h>		// For getopt
 
 
-/*
- * Main.
- */
-int
-main (int argc, char * argv[])
-{
-  int c;
-  int mode = 0;
-  int option_index = 0;
-  char * lang = "en";
+static const char *str_version = 
+    PACKAGE " " VERSION "\n"
+    "compiled at " __DATE__ "\n"
+    "datadir: " DATA_DIR "\n";
 
-  static struct option long_options[] = {
+static const char *str_usage = 
+    "Usage: wodox --help\n"
+    "Usage: wodox --version\n"
+    "Usage: wodox --edit <file>\n"
+    "Usage: wodox [<options>] [<file>...]\n";
+
+static const char *str_help = 
+    "\n"
+    "  -h, --help		Display this information and exit\n"
+    "  -v, --version		Display version info and exit\n"
+    "  -e, --edit <file>	Edit the specified level\n"
+    "  -l, --lang <lang>	Specify language\n"
+    "      --no-audio		Disable all audio\n"
+    "      --no-music		Disable all music\n"
+    "\n";
+
+static struct option long_options[] = {
     { "no-audio", no_argument, &enable_audio, 0 },
     { "no-music", no_argument, &enable_music, 0 },
     { "help", no_argument, 0, 'h' },
@@ -29,70 +39,66 @@ main (int argc, char * argv[])
     { "lang", required_argument, 0, 'l' },
     { "version", no_argument, 0, 'v' },
     { 0, 0, 0, 0 }
-  };
+};
 
-  while ((c = getopt_long (argc, argv, "hel:v", long_options, &option_index)) != -1)
-    switch (c)
-      {
-      case 0: // If this option set a flag do nothing.
-        break;
+/*
+ * Main.
+ */
+int
+main (int argc, char * argv[])
+{
+    int c;
+    int mode = 0;
+    int option_index = 0;
+    char * lang = "en";
 
-      case 'h':
-	printf ("Usage: %s [options] [file...]\n", argv[0]);
-	puts ("Options:");
-	puts ("  -h, --help            Display this information and exit.");
-	puts ("  -e, --edit            Edit instead of playing.");
-	puts ("  -l, --lang=LANG       Specify language.");
-	puts ("      --no-audio        Disable all audio.");
-	puts ("      --no-music        Disable music.");
-	puts ("  -v, --version         Display version info and exit.");
-	puts ("");
-	exit (EXIT_SUCCESS);
+    while ((c = getopt_long (argc, argv, "hvel:", long_options, &option_index)) != -1) {
+	switch (c) {
+	case 0: // If this option set a flag do nothing.
+            break;
 
-      case 'e':
-        mode = 1;
-	break;
+        case 'h':
+	    fputs(str_usage, stdout);
+	    fputs(str_help, stdout);
+	    exit(EXIT_SUCCESS);
+    
+        case 'e':
+            mode = 1;
+	    break;
+    
+        case 'l':
+            lang = optarg;
+	    break;
+    
+        case 'v':
+	    fputs(str_version, stdout);
+	    exit(EXIT_SUCCESS);
+    
+        default:
+	    fputs(str_usage, stdout);
+	    exit(EXIT_FAILURE);
+        }
+    }
 
-      case 'l':
-        lang = optarg;
-	break;
+    init_lang (lang);
+    init_graphics ();
 
-      case 'v':
-	puts (PACKAGE " " VERSION);
-	puts ("compiled at " __DATE__);
-	puts ("datadir: " DATA_DIR);
-	exit (EXIT_SUCCESS);
-      }
-
-  init_lang (lang);
-  init_graphics ();
-
-  if (optind < argc)
-    {
-      for (c = optind; c < argc; ++c)
-        {
-          if (mode == 0)
-            {
+    if (optind < argc) {
+	for (c = optind; c < argc; ++c) {
+	    if (mode == 0){
               if (play (argv[c], argv[c]) == 0)
-	        { 
 		  break; 
-		}
-   	    }
-          else
-            {
+   	    } else {
 	      edit (argv[c]);
             }
         }
-    }
-  else
-    {
-      menu ();
+    } else {
+	menu ();
     }
 
-  end_graphics ();
-  end_lang ();
-
-  return 0;
+    end_graphics ();
+    end_lang ();
+    return 0;
 }
 
 
